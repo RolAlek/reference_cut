fa6468d995961ca57cca6dc269110fed868952e0
Aleksandr Rol <rolaleksandr75@gmail.com>
2023-12-29 23:51:36 +0900

    default config sqlite

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ from os import getenv
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URI', 'value does not exist')
+    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = getenv('SECRET_KEY', 'value does not exist')
