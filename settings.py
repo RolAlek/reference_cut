33fc84fdcd12c7de35c6907ecff69f169033a48f
Aleksandr Rol <rolaleksandr75@gmail.com>
2023-12-29 23:48:29 +0900

    key

--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ from os import getenv
 class Config(object):
     SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = getenv('SECRET_KEY')
+    SECRET_KEY = getenv('SECRET_KEY', 'value does not exist')
